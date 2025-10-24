fad5a744da638a6364e3785d83ae66216ed8fa8f
JSep <89217843+JSep7@users.noreply.github.com>
2025-10-24 14:33:55 -0400

    changed index.js

--- backend/controllers/envelopes/index.js
+++ backend/controllers/envelopes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 router.use("/envelopes", require("./getEnvelopesRoute"));
+router.use("/envelopes", require("./postEnvelopeRoute"));
 
 // This routers to the index.js file in the controller folder
 module.exports = router;
