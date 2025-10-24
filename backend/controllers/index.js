7b49cd9b685bcb69edd3bcf0bcc1a657d47d143d
JSep <89217843+JSep7@users.noreply.github.com>
2025-10-24 14:35:51 -0400

    changed index.js in controller

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -12,6 +12,6 @@ router.get('/health', (req, res) => {
 router.use("/auth", require("./auth"));
 router.use("/user", require("./user"));
 // This routes from the index.js file in the envelope folder to the server index.js file
-router.use("/envelopes", require("./envelope"));
+router.use("/envelopes", require("./envelopes"));
 
 module.exports = router;
