852fb6e71df8466c7d44159c1b4cbc136067e264
Kathlea Corla <kathleacorla@yahoo.com>
2025-11-07 19:37:54 -0500

    deleteTransferRoute and getTransferByIdRoute added

--- backend/controllers/transfers/index.js
+++ backend/controllers/transfers/index.js
@@ -4,5 +4,7 @@ router.post('/', require('./postTransferRoute'));
 router.get('/', require('./getTransfersRoute')); // All transfers
 router.get('/filtered', require('./getFilteredTransfersRoute')); // Filtered transfers
 router.put('/:id', require('./putTransferRoute'));
+router.get('/:id', require('./getTransferByIdRoute'));
+router.delete('/:id', require('./deleteTransferRoute'));
 
 module.exports = router;
